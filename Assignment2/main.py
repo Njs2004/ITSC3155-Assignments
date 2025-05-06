2ea81dbe1bd51b3783cc14070755d78f35d4607e
Njs2004 <147880917+Njs2004@users.noreply.github.com>
2025-05-06 14:34:29 +0000

    Updated

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -2,18 +2,26 @@ import data
 from sandwich_maker import SandwichMaker
 from cashier import Cashier
 
-
-# Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
 
-
-
+sandwich_maker = SandwichMaker(resources)
+cashier = Cashier()
 
 def main():
-    ###  write the rest of the codes ###
+    while True:
+        choice = input("What size sandwich would you like? (small/medium/large/off): ").lower()
+        if choice == "off":
+            print("Turning off machine. Goodbye!")
+            break
+        elif choice in recipes:
+            sandwich = recipes[choice]
+            if sandwich_maker.check_resources(sandwich["ingredients"]):
+                payment = cashier.process_coins()
+                if cashier.transaction_result(payment, sandwich["cost"]):
+                    sandwich_maker.make_sandwich(choice, sandwich["ingredients"])
+        else:
+            print("Invalid selection.")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
